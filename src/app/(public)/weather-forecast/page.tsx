29662672e8e49ce70405c9c84250cc9efda003d4
phuocsanh <phuocsanhtps@gmail.com>
2026-01-08 19:15:47 +0700

    up

--- src/app/(public)/weather-forecast/page.tsx
+++ src/app/(public)/weather-forecast/page.tsx
@@ -22,7 +22,6 @@ import {
   DialogContent,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import dynamic from 'next/dynamic'
